65e71bdf1cac4279e4e97de925bbee813ea91fe1
João <joaoantoniobmx@hotmail.com>
2021-09-15 12:56:54 -0300

    criação de aplicativo blazor

---
+++ Estudos/Estudos/Estudos/Models/EstudoModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Estudos.Models
+{
+    public class EstudoModel
+    {
+        public int Id { get; set; }
+        public string Texto { get; set; }
+    }
+}
